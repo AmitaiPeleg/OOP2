6ae62884ae60549c47991cc6372bcfc3ddd0ad5c
AmitaiPeleg <amitaip10@gmail.com>
2025-03-16 11:44:30 +0200

    finish Q4

--- HW1/Q4/Rect.java
+++ HW1/Q4/Rect.java
@@ -1,5 +1,19 @@
 package Q4;
 
 public class Rect {
-    
+    private int length;
+    private int width;
+
+    public Rect(int l, int w) {
+        length = l;
+        width = w;
+    }
+
+    public int getArea() {
+        return length * width;
+    }
+
+    public int getCirc() {
+        return length * 2 + width * 2;
+    }
 }
--- HW1/Q4/Squer.java
+++ HW1/Q4/Squer.java
@@ -1,5 +1,8 @@
 package Q4;
-
+//  ריבוע יורש ממלבן
 public class Squer extends Rect {
-    
+    public Squer(int n){
+        super(n,n);
+    }
+
 }
