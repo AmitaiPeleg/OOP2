c1c0285424ebd4d658bf83b8f62875cb95474651
AmitaiPeleg <amitaip10@gmail.com>
2025-03-16 11:13:36 +0200

    check2

--- HW1/Q3/Program.java
+++ HW1/Q3/Program.java
@@ -2,8 +2,8 @@ package Q3;
 
 public class Program {
     public static void main(String[] args) {
-        XDate d1 = new XDate(30, 3, 2008);
-        XDate d2 = d1.addDays(11);
+        XDate d1 = new XDate(28, 2, 2007);
+        XDate d2 = d1.addDays(1);
         System.out.print(d2.getDate() + "/" + d2.getMonth() + "/" + d2.getYear());
     }
 
