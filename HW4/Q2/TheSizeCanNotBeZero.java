49dace35fcfda70d6613b5cab75e21d2c33582d7
AmitaiPeleg <amitaip10@gmail.com>
2025-05-12 16:56:27 +0300

    finish create exeptions

---
+++ HW4/Q2/TheSizeCanNotBeZero.java
@@ -0,0 +1,7 @@
+package HW4.Q2;
+
+public class TheSizeCanNotBeZero extends MatrixExeption {
+    public TheSizeCanNotBeZero(){
+        super("size of matrix can't be zero");
+    }
+}
---
+++ TheSizeCanNotBeNegativ.java
@@ -0,0 +1,8 @@
+import HW4.Q2.MatrixExeption;
+
+public class TheSizeCanNotBeNegativ extends MatrixExeption {
+    public TheSizeCanNotBeNegativ() {
+        super("size of matrix can't be negativ");
+
+    }
+}
