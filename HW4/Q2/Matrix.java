f6c6f6eb0845c10457985b98f631e3d834a3dd33
AmitaiPeleg <amitaip10@gmail.com>
2025-05-12 22:13:38 +0300

    just fix

--- HW4/Q2/Matrix.java
+++ HW4/Q2/Matrix.java
@@ -3,5 +3,29 @@ import HW3.Q1.Arithmetic;
 
 public class Matrix implements Arithmetic {
     int[][] data;
+
+    @Override
+    public Arithmetic add(Object other) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public Arithmetic sub(Object other) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sub'");
+    }
+
+    @Override
+    public Arithmetic mul(Object other) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mul'");
+    }
+
+    @Override
+    public Arithmetic div(Object other) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'div'");
+    }
     
 }
--- HW4/Q2/TheSizeCanNotBeNegativ.java
+++ HW4/Q2/TheSizeCanNotBeNegativ.java
@@ -1,5 +1,4 @@
-import HW4.Q2.MatrixExeption;
-
+package HW4.Q2;
 public class TheSizeCanNotBeNegativ extends MatrixExeption {
     public TheSizeCanNotBeNegativ() {
         super("size of matrix can't be negativ");
